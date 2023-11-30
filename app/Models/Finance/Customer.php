a1b36f056df163dd4c34220a1e7ed711229481c7
Firman Maulana <firmanasgani37@gmail.com>
2023-11-30 18:13:43 +0700

    add API for Customer

---
+++ app/Models/Finance/Customer.php
@@ -0,0 +1,51 @@
+<?php
+    namespace App\Models\Finance;
+    use Illuminate\Database\Eloquent\Model;
+    use DB;
+
+    class Customer extends Model {
+
+        function see_customers($cari) {
+            $where='';
+            if($cari!=null || $cari!='') {$where=" WHERE Perusahaan LIKE '%$cari%'";}
+            $get = DB::Connection('mysql_fna')->select("SELECT CustID, DeviceID, KodePerusahaan, Perusahaan, npwp, KontakPerson, Telephone, MataAnggaran FROM acustomer $where");
+            if(count($get) == 0) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Data kosong',
+                    'title' => 'Gagal',
+                    'data' => ''
+                ]);
+            }else{
+                return response()->json([
+                    'status' => 'success',
+                    'message' => 'Data berhasil diambil',
+                    'title' => 'Berhasil',
+                    'data' => $get
+                ]);
+            }
+
+        }
+
+        function  add_customer() {
+
+        }
+
+        function update_customer() {
+            
+        }
+
+        function delete_customer($id) {
+            $total = count($id);
+            for($i=0; $i<$total; $i++) {
+                $delete =DB::Connection('mysql_fna')->select("DELETE FROM provider WHERE CustID='$id[$i]'");   
+            }
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Berhasil menghapus data Customer',
+                'title' => 'Berhasil'
+            ]);
+        }
+    }
+?>
