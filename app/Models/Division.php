bfc44a6477a546506ad5151b5ad5def207447c75
Firman Maulana <firmanasgani37@gmail.com>
2023-12-05 15:25:23 +0700

    add model for division

---
+++ app/Models/Division.php
@@ -0,0 +1,54 @@
+<?php
+    namespace App\Models;
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Support\Facades\DB;
+
+    class Division extends Model {
+
+        function see_division() {
+            $get = DB::table('division')->where('deleted', 0)->get();
+            if($get == 0) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Sorry, nothing found',
+                    'title' => 'We don\'t have data'
+                ]);
+            }else {
+                return response()->json([
+                    'status' => 'success',
+                    'message' => 'Data has been loaded successfully',
+                    'title' => 'We have the data'
+                ]);
+            }
+
+        }
+
+        function search_division($search, $dirs) {
+            $where = '';
+            if($search != null || $search != '') {
+                $where = " AND (divisionName LIKE '%$search%') OR (divisiCode LIKE '%$search%')'"; 
+            }
+            if($dirs != null || $dirs != '') {
+                $where = " AND dirId=$dirs"; 
+            }
+
+            $query = "SELECT * FROM division WHERE deleted=0 $where ORDER BY divisionName ASC";
+            $get = DB::select($query);
+            if($get == 0) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Sorry, nothing found',
+                    'title' => 'We don\'t have data'
+                ]);
+            }else {
+                return response()->json([
+                    'status' => 'success',
+                    'message' => 'Data has been loaded successfully',
+                    'title' => 'We have the data'
+                ]);
+            }
+
+        }
+    }
+
+?>
