33b13584136e374c50eca1ec095574f061d0996c
Firman Maulana <firmanasgani37@gmail.com>
2023-12-12 16:16:45 +0700

    commit sui.....

--- components/ticket/myticket.php
+++ components/ticket/myticket.php
@@ -2,48 +2,39 @@
 
 <div class="container-xxl flex-grow-1 container-p-y">
     <div class="row mb-3">
-        <div class="col-lg-4 order-0 mb-3">
+        <div class="col-lg-3 order-0 mb-3">
             <div class="card">
                 <div class="card-body">
                     <p> yang telah dibuat</p>
                     <h3>6</h3>
                 </div>
-                <div class="card-footer">
-                    <button class="btn btn-link">Lihat detail</button>
-                </div>
+              
             </div>
         </div> 
-        <div class="col-lg-4 order-0 mb-3">
+        <div class="col-lg-3 order-0 mb-3">
             <div class="card">
                 <div class="card-body">
                     <p>Yang sudah diassign</p>
                     <h3>6</h3>
                 </div>
-                <div class="card-footer">
-                    <button class="btn btn-link">Lihat detail</button>
-                </div>
+                
             </div>
         </div>
-        <div class="col-lg-4 order-0 mb-3">
+        <div class="col-lg-3 order-0 mb-3">
             <div class="card">
                 <div class="card-body">
                     <p> yang sedang postpone</p>
                     <h3>6</h3>
                 </div>
-                <div class="card-footer">
-                    <button class="btn btn-link">Lihat detail</button>
-                </div>
+              
             </div>
         </div>
 
-        <div class="col-lg-4 order-0 mb-3">
+        <div class="col-lg-3 order-0 mb-3">
             <div class="card">
                 <div class="card-body">
                     <p> yang telah ditutup</p>
-                    <h3>6</h3>
-                </div>
-                <div class="card-footer">
-                    <button class="btn btn-link">Lihat detail</button>
+                    <h3 >6</h3>
                 </div>
             </div>
         </div>
