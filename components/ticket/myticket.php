78c18cc2ccd462b612aa23fbeefe28f683e8230f
Firman Maulana <firmanasgani37@gmail.com>
2023-12-12 15:18:40 +0700

    add some card in my ticket

--- components/ticket/myticket.php
+++ components/ticket/myticket.php
@@ -1,40 +1,50 @@
 <?php $user = isset($_GET['user']) ? $_GET['user'] : '';?>
 
 <div class="container-xxl flex-grow-1 container-p-y">
-    <div class="row">
-        <div class="col-lg-12 mb-4 order-0">
+    <div class="row mb-3">
+        <div class="col-lg-4 order-0 mb-3">
             <div class="card">
-                <div class="card-header bg-primary">
-                    <h4 class="text-title text-white">Data Status Karyawan</h4>
+                <div class="card-body">
+                    <p> yang telah dibuat</p>
+                    <h3>6</h3>
                 </div>
-                <main class="card-body mt-3">
-                        <div class="col-auto mb-3">
-                            <input type="text" class="form-control form-control-sm" placeholder="Cari" id="cari">
-                        </div>
-                        <content class="col-md-12 mb-3">
+                <div class="card-footer">
+                    <button class="btn btn-link">Lihat detail</button>
+                </div>
+            </div>
+        </div> 
+        <div class="col-lg-4 order-0 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <p>Yang sudah diassign</p>
+                    <h3>6</h3>
+                </div>
+                <div class="card-footer">
+                    <button class="btn btn-link">Lihat detail</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 order-0 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <p> yang sedang postpone</p>
+                    <h3>6</h3>
+                </div>
+                <div class="card-footer">
+                    <button class="btn btn-link">Lihat detail</button>
+                </div>
+            </div>
+        </div>
 
-                            <caption class="text-muted small-text">Total: <span id="total">0</span></caption>
-                            <div class="table-responsive" style="height: 400px">
-                            
-                                <table class="table table-striped table-hover">
-                                    <thead style="background: white; position: sticky; top: 0;box-shadow: 0 2px 2px -1px rgba(0, 0, 0, 0.4);">
-                                        <th class="col">#</th>
-                                        <th class="col">Kode</th>
-                                        <th class="col">Nama</th>
-                                    </thead>
-                                    <tbody id="tbl-status"></tbody>
-                                </table>
-                            </div>
-                        </content>
-                    </div>
-                </main>
-                
-                <footer class="card-footer mt-3">
-                    <button type="button" class="btn btn-primary btn-sm" id="addStatus">Tambah</button>
-                    <button type="button" class="btn btn-info btn-sm" id="updateStatus">Ubah</button>
-                    <button type="button" class="btn btn-danger btn-sm" id="deleteStatus">Hapus</button>
-                    <button type="button" class="btn btn-secondary btn-sm" id="backto">Kembali</button>
-                </footer>
+        <div class="col-lg-4 order-0 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <p> yang telah ditutup</p>
+                    <h3>6</h3>
+                </div>
+                <div class="card-footer">
+                    <button class="btn btn-link">Lihat detail</button>
+                </div>
             </div>
         </div>
     </div>
